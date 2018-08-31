bcc77a08e0ea860b876a73ab945f31e1858b7f50
Draqneel <marabucavi@gmail.com>
2018-08-31 23:32:12 +0300

    add Queue implementation

---
+++ Queue/src/ru/ivmiit/Queue.java
@@ -0,0 +1,48 @@
+package ru.ivmiit;
+
+public class Queue {
+    private Node root;
+    private Node tail;
+    private Node temp;
+
+    public Node element() {
+        if (root != null) {
+            return root;
+        } else {
+            System.err.println("NoSuchElementException");
+        }
+        return null;
+    }
+
+    public void push (Node node) {
+        if (root == null) {
+            root = node;
+            tail = node;
+        } else {
+            temp = tail;
+            temp.setLink(node);
+            tail = node;
+        }
+    }
+
+    public Node peek () {
+        return root;
+    }
+
+    public Node poll () {
+        temp = root;
+        root = root.getLink();
+        return temp;
+    }
+
+    public Node remove () {
+        if (root != null) {
+            temp = root;
+            root = root.getLink();
+            return temp;
+        } else {
+            System.err.println("NoSuchElementException");
+        }
+        return null;
+    }
+}
