4770d51fbb81dc784decc90396a272f9e1766ae0
Draqneel <marabucavi@gmail.com>
2018-09-01 16:25:45 +0300

    add Task 1

---
+++ Task 1 (Codewars Two to One)/src/ru/ivmiit/Main.java
@@ -0,0 +1,41 @@
+package ru.ivmiit;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add 1st string");
+        String a = scanner.nextLine();
+        System.out.println("Add 2nd string");
+        String b = scanner.nextLine();
+        System.out.println(twoToOne(a, b));
+    }
+
+    public static String twoToOne(String a, String b) {
+        final int MIN_VALUE = 97; // "a" in ASCII
+        final int MAX_VALUE = 122; // "z" in ASCII
+        final int MAX_ITERATION_COUNT = 26; // count of letters in ABC
+        String string = a + b;
+        String result = "";
+        int test = MIN_VALUE;
+        char[] array = string.toCharArray();
+        for (int i = 0; i < MAX_ITERATION_COUNT; i++) {
+            if (array[i] < MIN_VALUE || array[i] > MAX_VALUE) {
+                return "incorrect string";
+            }
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] == test) {
+                    result = result + array[j];
+                    break;
+                }
+            }
+            test++;
+            if (test > MAX_VALUE) {
+                break;
+            }
+        }
+        return result;
+    }
+}
