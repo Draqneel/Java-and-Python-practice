d474957be6b21f96426f4e1e9a6f4c77f3520cab
Draqneel <marabucavi@gmail.com>
2018-09-14 16:14:31 +0300

    add documetation

--- Task 3 (Codewars Find The Parity Outlier)/src/ru/ivmiit/Main.java
+++ Task 3 (Codewars Find The Parity Outlier)/src/ru/ivmiit/Main.java
@@ -1,10 +1,20 @@
 package ru.ivmiit;
 
+/**
+ * SPECIFICATION
+ * You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+ * The array is either entirely comprised of odd integers or entirely comprised of even integers except
+ * for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+ */
 public class Main {
 
     public static void main(String[] args) {
     }
 
+    /**
+     * @param array - verifiable array
+     * @return int variable - "outlier" N
+     */
     public static int findOutlier (int[] array){
         boolean isEven = isEven(array);
         if (isEven == true) {
@@ -20,9 +30,14 @@ public class Main {
                 }
             }
         }
+        // default output
         return 0;
     }
 
+    /**
+     * @param array - verifiable array
+     * @return boolean variable - array is even or not
+     */
     public static boolean isEven(int[] array){
         int countEven = 0;
         int countOdd = 0;
@@ -39,8 +54,3 @@ public class Main {
         return false;
     }
 }
-/* SPECIFICATION
-* You are given an array (which will have a length of at least 3, but could be very large) containing integers.
-* The array is either entirely comprised of odd integers or entirely comprised of even integers except
-* for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
- */
