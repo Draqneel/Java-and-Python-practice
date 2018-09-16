c015fddb3f21c592f2791b2013dbc8418caf3748
Draqneel <marabucavi@gmail.com>
2018-09-16 23:57:47 +0300

    add comments and documentation

--- SimpleSort/src/ru/ivmiit/Main.java
+++ SimpleSort/src/ru/ivmiit/Main.java
@@ -1,5 +1,11 @@
 package ru.ivmiit;
 
+/**
+ * SPECIFICATION
+ * wiki: https://en.wikipedia.org/wiki/Selection_sort
+ *
+ * @author Draqneel
+ */
 public class Main {
 
     public static void main(String[] args) {       
@@ -12,12 +18,14 @@ public class Main {
         int temp;
         int ilast = array.length - 1;
         for (int i = 0; i < array.length; i++) {
+            // find max value in array
             for (int j = 0; j + i < array.length; j++) {
                 if (array[j] > max) {
                     max = array[j];
                     imax = j;
                 }
             }
+            // swap max value with last element
             temp = array[ilast];
             array[ilast] = max;
             array[imax] = temp;
