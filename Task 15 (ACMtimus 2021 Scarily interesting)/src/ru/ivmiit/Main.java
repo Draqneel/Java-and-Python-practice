0bc817370f66a6cc2eb7a17b70c4229f246d341b
Draqneel <marabucavi@gmail.com>
2018-09-21 12:06:54 +0300

    Add Task 15 with specification

--- Task 15 (ACMtimus 2021 Scarily interesting)/src/ru/ivmiit/Main.java
+++ Task 15 (ACMtimus 2021 Scarily interesting)/src/ru/ivmiit/Main.java
@@ -20,49 +20,75 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
         scarilyInteresting();
     }
 
+    /**
+     * Main method of Task
+     */
     public static void scarilyInteresting() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Add N please");
+        // read n
         int n = scanner.nextInt();
+        // index of searching element
         int indexOne = 0;
         int indexTwo = 0;
+        // counter of outputs
         int count = 0;
+        // two flags - has or not value in 1st and 2nd array
         boolean flagOne = false;
         boolean flagTwo = false;
+        // input arrays
         int[] arrayOne = new int[n];
         int[] arrayTwo = new int[n];
+        // fill the 1st array
         for (int i = 0; i < n; i++) {
             System.out.println("Add " + (i + 1) + " element for 1st team");
             arrayOne[i] = scanner.nextInt();
         }
+        // fill the 2nd array
         for (int i = 0; i < n; i++) {
             System.out.println("Add " + (i + 1) + " element for 2st team");
             arrayTwo[i] = scanner.nextInt();
         }
+        // searching and output equals elements
         for (int i = 6; i >= 0; i--) {
+            // if count of outputs equals n - break the llop
             if (count == n) {
                 break;
             }
             for (int j = 0; j < n; j++) {
                 if (arrayOne[j] == i) {
                     flagOne = true;
-                    indexOne = j + 1;
+                    indexOne = j;
                 }
                 if (arrayTwo[j] == i) {
                     flagTwo = true;
-                    indexTwo = j + 1;
+                    indexTwo = j;
                 }
             }
+            // if an element was found in two arrays - output the couple
             if (flagOne == true && flagTwo == true) {
-                System.out.println(indexOne + " " + indexTwo);
+                System.out.println((indexOne + 1) + " " + (indexTwo + 1));
                 count++;
+                arrayOne[indexOne] = -1;
+                arrayTwo[indexTwo] = -1;
             }
             flagOne = false;
             flagTwo = false;
+        }
+        // output another couples
+        for (int i = 0; i < n - count; i++) {
+            for (int j = 0; j < arrayOne.length; j++) {
+                if (arrayOne[j] != -1) {
+                    for (int k = 0; k < arrayTwo.length; k++) {
+                        if (arrayTwo[k] != -1) {
+                            System.out.println((j + 1) + " " + (k + 1));
+                        }
+                    }
+                }
+            }
         }
     }
 }
