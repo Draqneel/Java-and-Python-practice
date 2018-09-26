4a3af9ddcb1765540f9e537eac04812222a7594d
Draqneel <marabucavi@gmail.com>
2018-09-26 23:51:15 +0300

    Add layout and specification for Task 17

---
+++ Task 17 (Codewars Dashatize it)/src/ru/ivmiit/Main.java
@@ -0,0 +1,23 @@
+package ru.ivmiit;
+
+/**
+ * SPECIFICATION
+ * Given a number, return a string with dash'-'marks before and after each odd integer,
+ * but do not begin or end the string with a dash mark.
+ * Ex:
+ * dashatize(274) -> '2-7-4'
+ * dashatize(6815) -> '68-1-5'
+ *
+ * @author Draqneel 
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static String dashatize(int num) {
+        String result = "";
+        return result;
+    }
+}
