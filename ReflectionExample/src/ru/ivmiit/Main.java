1df559b0c382450ad7a8b0744268935af7885ab8
Draqneel <marabucavi@gmail.com>
2019-01-26 18:41:38 +0300

    add final version of code

--- ReflectionExample/src/ru/ivmiit/Main.java
+++ ReflectionExample/src/ru/ivmiit/Main.java
@@ -1,15 +1,13 @@
 package ru.ivmiit;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
+import java.lang.reflect.*;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        SomeClass someClass = new SomeClass("Hello, John");
+        SomeClass someClass = new SomeClass();
         // Ways to get Class class:
         // method getClass, static variable class, load class by path
         Class clss = someClass.getClass();
@@ -18,11 +16,28 @@ public class Main {
         // create class without "new"
         SomeClass someClass1 = (SomeClass) clss.newInstance();
         // get methods/constructors
-        Constructor[] constructors = clss.getConstructors();
-        Constructor[] decConstructors = clss.getDeclaredConstructors();
-        Method[] methods = clss.getMethods();
-        Method[] decMethods = clss.getDeclaredMethods();
+        Constructor[] constructors = clss.getDeclaredConstructors();
+        Method[] methods = clss.getDeclaredMethods();
+        System.out.println("methods:");
+        for (Method method: methods){
+            System.out.println(Modifier.toString(method.getModifiers()));
+            System.out.println(method.getReturnType().getName());
+        }
+        System.out.println();
+        System.out.println("fields:");
+        Field[] fields = clss.getDeclaredFields();
+        for(Field field: fields){
+            System.out.println(field.getName());
+            System.out.println(field.getType().getName());
+            System.out.println(Modifier.toString(field.getModifiers()));
+            field.setAccessible(true);
+            System.out.println(field.getInt(clss));
+        }
+//        Constructor[] decConstructors = clss.getDeclaredConstructors();
+//        Method[] methods = clss.getMethods();
+//        Method[] decMethods = clss.getDeclaredMethods();
 
+        System.out.println("constructors:");
         for (Constructor c : constructors) {
             System.out.println(c.getName());
             Parameter[] parameters = c.getParameters();
@@ -36,16 +51,20 @@ public class Main {
 
 class SomeClass {
     private static transient int i;
-    String s;
+//    String s;
 
     SomeClass() {
     }
 
-    SomeClass(String s) {
-        this.s = s;
-    }
+//    SomeClass(String s) {
+//        this.s = s;
+//    }
+//
+//    public String getI() {
+//        return s;
+//    }
 
-    public String getS() {
-        return s;
+    public synchronized void newMetod(){
+        System.out.println("some method");
     }
 }
